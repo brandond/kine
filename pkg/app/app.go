a2456ebe9006010f278f4b17e821be12d45890b4
Brad Davidson <brad.davidson@rancher.com>
2026-05-26 07:58:30 +0000

    Minor logging enhancements
    split debug+trace to stdout; add client IP to GRPC logs
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,6 +2,8 @@ package app
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"sync"
 	"time"
 
@@ -10,6 +12,7 @@ import (
 	"github.com/k3s-io/kine/pkg/signals"
 	"github.com/k3s-io/kine/pkg/version"
 	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/writer"
 	"github.com/urfave/cli/v2"
 )
 
@@ -240,6 +243,11 @@ func run(c *cli.Context) (rerr error) {
 	if c.Bool("debug") {
 		logrus.SetLevel(logrus.TraceLevel)
 	}
+
+	// send info/error/warning to stderr, debug/trace to stdout
+	logrus.SetOutput(ioutil.Discard)
+	logrus.AddHook(&writer.Hook{Writer: os.Stderr, LogLevels: []logrus.Level{logrus.PanicLevel, logrus.FatalLevel, logrus.ErrorLevel, logrus.WarnLevel, logrus.InfoLevel}})
+	logrus.AddHook(&writer.Hook{Writer: os.Stdout, LogLevels: []logrus.Level{logrus.DebugLevel, logrus.TraceLevel}})
 
 	ctx := signals.SetupSignalContext()
 
